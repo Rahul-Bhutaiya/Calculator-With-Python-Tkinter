bc5fc6fec7b165158578fe0b7a383bbe07ff73bd
Rahul Bhutaiya <111126404+Rahul-Bhutaiya@users.noreply.github.com>
2022-12-09 23:30:55 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -6,14 +6,14 @@ obj.geometry('600x400')
 obj.iconbitmap('icons8-apple-logo-16 (1).ico')
 
 
-lbl1=Label(obj,text='Value 1 :')
+lbl1=Label(obj,text='Value 1 :',fg='red')
 lbl1.place(x=200,y=100)
-e1=Entry(obj)
+e1=Entry(obj,bg='yellow')
 e1.place(x=250,y=100)
 
-lbl2=Label(obj,text='Value 2 :')
+lbl2=Label(obj,text='Value 2 :',fg='red')
 lbl2.place(x=200,y=130)
-e2=Entry(obj)
+e2=Entry(obj,bg='yellow')
 e2.place(x=250,y=130)
 
 lbl3=Label(obj,text='',fg='purple')
@@ -33,16 +33,16 @@ def division():
     lbl3.config(text='Division is : '+str(eval(e1.get())/eval(e2.get())))
 
 
-sum=Button(obj,text='+',command=add)
+sum=Button(obj,text='+',command=add,bg='yellow',fg='red',activebackground='black',activeforeground='purple')
 sum.place(x=250,y=160)
 
-sub=Button(obj,text='-',command=min)
+sub=Button(obj,text='-',command=min,bg='yellow',fg='red',activebackground='black',activeforeground='purple')
 sub.place(x=280,y=160)
 
-mul=Button(obj,text='*',command=product)
+mul=Button(obj,text='*',command=product,bg='yellow',fg='red',activebackground='black',activeforeground='purple')
 mul.place(x=310,y=160)
 
-div=Button(obj,text='/',command=division)
+div=Button(obj,text='/',command=division,bg='yellow',fg='red',activebackground='black',activeforeground='purple')
 div.place(x=340,y=160)
 
 
